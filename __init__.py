5b92670f0922762edaca520b22512a8a0f0869ca
Etahn-C <coutoethan@outlook.com>
2026-07-24 22:25:14 -0400

    Small changes to plugin

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
-from calibre import EditBookToolPlugin
+from calibre.customize import InterfaceActionBase
 
 
-class FimfictionEbookPlugin(EditBookToolPlugin):
+class FimfictionEbookPlugin(InterfaceActionBase):
 
     name = 'Fimfiction Ebook Plugin'
     version = (1, 0, 0)
     author = 'EveCouto'
     supported_platforms = ['windows']
     description = 'Allows for fixing images and merging updated epubs'
-    minimum_calibre_version = (1, 46, 0)
+    minimum_calibre_version = (9, 11, 0)
---
+++ plugin.py
@@ -0,0 +1,9 @@
+from calibre.gui2.actions import InterfaceAction
+
+
+class FimfictionInterfaceAction(InterfaceAction):
+    name = "fimfiction-tool"
+
+
+def genesis(self):
+    ...
