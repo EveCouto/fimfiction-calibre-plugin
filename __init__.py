0284117d508a267e3e3c4a175fbb37d148534b18
Etahn-C <coutoethan@outlook.com>
2026-07-28 12:14:02 -0400

    Updated to 1.0.1

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from calibre.customize import InterfaceActionBase
 class FimfictionEbookPlugin(InterfaceActionBase):
 
     name = 'Fimfiction Ebook Plugin'
-    version = (1, 0, 0)
+    version = (1, 0, 1)
     author = 'EveCouto'
     supported_platforms = ['windows']
     description = 'Allows for fixing images and merging updated epubs'
